bcb6fb72e4b682e915a757274dcc2a732abda731
Fabricio Baliño <fabricio.balino@gmail.com>
2023-09-28 10:25:48 -0300

    Fixed slides bug

--- JS/slideshow.js
+++ JS/slideshow.js
@@ -1,8 +1,20 @@
-const images = [
+let images;
+
+if (window.location.pathname === "/index.html") {
+  images = [
+    "/Images/EmpresaNatural.png",
+    "/Images/ShowCase1.png",
+    "/Images/ShowCase2.png",
+  ];
+} else if (window.location.pathname === "/Pages/HomeEsp.html") {
+  images = [
     "../Images/EmpresaNatural.png",
     "../Images/ShowCase1.png",
     "../Images/ShowCase2.png",
   ];
+} else {
+  images = [];
+}
   
   let currentIndex = 0;
   const imageElement = document.getElementById("image3");
@@ -10,22 +22,18 @@ const images = [
   function nextImage() {
     currentIndex = (currentIndex + 1) % images.length;
     
-    // Apply opacity transition for fade out
     imageElement.style.opacity = 0;
     
     setTimeout(() => {
       imageElement.src = images[currentIndex];
       
-      // Reset opacity and apply transform transition for sliding in
       setTimeout(() => {
         imageElement.style.opacity = 1;
         imageElement.style.transform = "translateX(0)";
       }, 500);
       
-      // Apply transform to slide in from the right
       imageElement.style.transform = "translateX(100%)";
-    }, 500); // Adjust the timing to match your transition duration
+    }, 500);
   }
-  
-  // Change the image every 3 seconds (adjust as needed)
+
   setInterval(nextImage, 6000);
